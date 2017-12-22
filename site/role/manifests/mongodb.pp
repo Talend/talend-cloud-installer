a825acec3d0939775241e1b228a27c1159bcf9dc
Piotr Kieszczyński <piotr.kieszczynski@nordcloud.com>
2017-12-22 15:10:51 +0100

    Test if classes are executed in order

--- site/role/manifests/mongodb.pp
+++ site/role/manifests/mongodb.pp
@@ -7,6 +7,8 @@ class role::mongodb {
   include ::profile::common::hosts
   include ::profile::mongodb
 
+  Class['::profile::base'] -> Class['::profile::common::hosts'] -> Class['::profile::mongodb']
+
   role::register_role { 'mongodb': }
 
 }
