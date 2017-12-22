4ebea16ee144f114e00fb93de05781b5ff6686a3
Piotr Kieszczyński <piotr.kieszczynski@nordcloud.com>
2017-12-22 16:55:11 +0100

    Revert "Test if classes are executed in order"
    This reverts commit 210f5caa14f680c902abb934092a314468c17746.

--- site/role/manifests/mongodb.pp
+++ site/role/manifests/mongodb.pp
@@ -7,8 +7,6 @@ class role::mongodb {
   include ::profile::common::hosts
   include ::profile::mongodb
 
-  Class['::profile::base'] -> Class['::profile::common::hosts'] -> Class['::profile::mongodb']
-
   role::register_role { 'mongodb': }
 
 }
