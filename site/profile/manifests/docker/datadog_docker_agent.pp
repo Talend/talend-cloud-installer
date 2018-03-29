699d2974d6c3fd55b62c1b328bdfbd66a1608ef5
root <ajadhav@talend.com>
2018-03-28 18:02:11 -0700

    changed log level for debug

--- site/profile/manifests/docker/datadog_docker_agent.pp
+++ site/profile/manifests/docker/datadog_docker_agent.pp
@@ -14,15 +14,15 @@ class profile::docker::datadog_docker_agent (
   docker::run { 'datadog-docker-agent':
     running => $running,
     image   => $image,
-    net     => 'host',
     volumes => [
       '/var/run/docker.sock:/var/run/docker.sock:ro',
       '/proc/:/host/proc/:ro',
       '/cgroup/:/host/sys/fs/cgroup:ro'
     ],
     env     => [
-      'LOG_LEVEL=INFO'
+      'LOG_LEVEL=debug'
     ]
+    
   }
 
 }
