3112031af9eadcf8f5ffae19c25fb55773630b88
root <ajadhav@talend.com>
2018-04-02 10:21:50 -0700

    added dd backend

--- site/profile/manifests/docker/datadog_docker_agent.pp
+++ site/profile/manifests/docker/datadog_docker_agent.pp
@@ -20,7 +20,8 @@ class profile::docker::datadog_docker_agent (
     ],
     env     => [
       "API_KEY=${dd_agent_key}",
-      'LOG_LEVEL=info'
+      'LOG_LEVEL=info',
+      'SD_BACKEND=docker'
     ]
   }
 
