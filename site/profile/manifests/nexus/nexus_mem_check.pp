f1bad72c58802f4b5f3e5060bfc31438329c662a
root <ajadhav@talend.com>
2018-10-03 15:04:01 -0700

    script to restart nexus

---
+++ site/profile/manifests/nexus/nexus_mem_check.pp
@@ -0,0 +1,21 @@
+# this is class to check nexus memory and restart nexus service if memory is below 500mb and other two instances under elb are in-service
+
+class profile::nexus::nexus_mem_check(
+  $nexus_cron_hours = '*',
+  $nexus_cron_minute = '10'
+){
+  file { '/usr/local/bin/nexus_mem_check.sh':
+    source  => 'puppet:///modules/profile/usr/local/bin/nexus_mem_check.sh',
+    mode    => '0755',
+    owner   => 'root',
+    require => Package[awscli];
+  }
+  cron{
+    'nexus_memory_check':
+      ensure  => 'present',
+      command => '/usr/local/bin/nexus_mem_check.sh',
+      user    => 'root',
+      hour    => $nexus_cron_hours,
+      minute  => $nexus_cron_minute
+  }
+}
