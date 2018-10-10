6de4af6b36761a8947b427e1c8c50a5c928a7afa
root <ajadhav@talend.com>
2018-10-09 22:56:16 -0700

    change in cron time

--- site/profile/manifests/nexus/nexus_mem_check.pp
+++ site/profile/manifests/nexus/nexus_mem_check.pp
@@ -3,14 +3,21 @@
 class profile::nexus::nexus_mem_check(
   $nexus_cron_hours = '*',
   $nexus_cron_minute = '*/10'
+  $ec2_userdata = pick_default($::ec2_userdata, '')
+  if $ec2_userdata =~ /InstanceA/ {
+    $nexus_cron_minute = '0,10,20,30,40,50'
+  } elseif ($ec2_userdata =~ /InstanceB/) {
+    $nexus_cron_minute = '3,13,23,33,43,53'
+  } else {
+    $nexus_cron_minute = '6,16,26,36,46,56'
+  }
 ){
   if hiera('profile::nexus_restart_cron::enable'){
     file { '/usr/local/bin/nexus_mem_check.sh':
-      source  => 'puppet:///modules/profile/usr/local/bin/nexus_mem_check.sh',
-      mode    => '0755',
-      owner   => 'root',
-      group   => 'root',
-      require => Package[awscli];
+      source => 'puppet:///modules/profile/usr/local/bin/nexus_mem_check.sh',
+      mode   => '0755',
+      owner  => 'root',
+      group  => 'root'
     }
     cron{
       'nexus_memory_check':
